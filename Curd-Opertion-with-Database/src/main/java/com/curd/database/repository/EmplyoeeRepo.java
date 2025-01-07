5ab9aa8843dcc6bd98b6dd198fbd3abdfb23fa72
RajSharma-java <rajsharma2278608@gmail.com>
2025-01-07 19:36:45 +0530

    Delete Curd-Opertion-with-Database directory

--- Curd-Opertion-with-Database/src/main/java/com/curd/database/repository/EmplyoeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.curd.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curd.database.entity.Emplyoee;
-
-public interface EmplyoeeRepo extends JpaRepository<Emplyoee, String> {
-
-}
--- Curd-Opertion-with-Database/src/main/java/com/curd/database/service/RajService.java
+++
@@ -1,9 +0,0 @@
-package com.curd.database.service;
-
-import com.curd.database.dao.RajDao;
-
-public interface RajService  {
-	
-	RajDao create(RajDao dao );
-	
-}
